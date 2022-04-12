96731df10944c82ecc861a86ed473a4760d2e83c
RiccardoBeca <riccardobeca@gmail.com>
2022-04-12 15:05:32 +0100

    init

---
+++ jsnack1/script.js
@@ -0,0 +1,13 @@
+/*
+ JSnack 1
+ L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
+
+
+ 1.creo due prompt per chiedere all'utente di inserire due numeri
+ 2.if statement per dichiarare il maggiore tra i due numeri inseriti
+ 3.stampo il numero maggiore con innerHTML
+
+*/
+
+
+
