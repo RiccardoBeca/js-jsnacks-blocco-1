2e73fd0c96e8a1fe08f2ab5066948846e16a9d64
RiccardoBeca <riccardobeca@gmail.com>
2022-04-14 16:26:40 +0100

    jsnack3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -4,3 +4,13 @@
  Il programma stampa la somma di tutti i numeri inseriti.
  Esegui questo programma in due versioni, con il for e con il while (facoltativo)
 */
+
+totalSum = 0;
+
+for (let i = 0; i < 5; i++) {
+  let userNumber = parseInt(prompt("Inserisci un numero"));
+  totalSum += userNumber;
+  console.log(totalSum);
+}
+
+document.getElementById("output-text").innerHTML = totalSum;
